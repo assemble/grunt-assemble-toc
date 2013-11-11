b1a44b1b694d501f2b0f5d8621fd4769e31d1d8a
Jon Schlinkert <dev@sellside.com>
2013-11-11 03:24:20 -0500

    adds options for customizing TOC classes

--- index.js
+++ index.js
@@ -23,26 +23,33 @@ var cheerio = require('cheerio');
  * @param  {Function} callback
  */
 module.exports = function(params, callback) {
-
   'use strict';
+
+  var opts = params.assemble.options;
+  opts.toc = opts.toc || {};
+
+  // id to use to append TOC
+  var id = '#' + (opts.toc.id || 'toc');
+  var modifier = opts.toc.modifier || '';
+  var li = opts.toc.li ? (' class="' + opts.toc.li + '"') : '';
 
   // load current page content
   var $ = cheerio.load(params.content);
-  var toc = cheerio.load('<ul></ul>');
+  var toc = cheerio.load('<ul class="toc ' + modifier + '"></ul>');
 
   // get all the anchor tags from inside the headers
   var anchors = $('h1 a[name],h2 a[name],h3 a[name],h4 a[name]');
   anchors.map(function(i, e) {
     var item = [
-      '<li>',
+      '<li' + li + '>',
       '   <a href="#' + e.attribs.name + '">' + $(e.parent).text().trim() + '</a>',
-      '</li>',
-      ''
+      '</li>'
     ].join('\n');
     toc('ul').append(item);
   });
 
-  $('body').prepend(toc.html());
+  $(id).append(toc.html());
+
   params.content = $.html();
   callback();
 };
