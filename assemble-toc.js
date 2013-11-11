0b1e028b16efbf6e38b28c2b8d3f738f06d7ef0e
Jon Schlinkert <dev@sellside.com>
2013-11-10 22:24:40 -0500

    removes unused vars and deps, updates metadata, plugin repo name

--- assemble-toc.js
+++ assemble-toc.js
@@ -1,9 +1,9 @@
 /**
-* Assemble Plugin: Toc
-* https://github.com/assemble/assemble-plugin-toc
+* Assemble Plugin: TOC
+* https://github.com/assemble/assemble-contrib-toc
 *
 * Copyright (c) 2013 Brian Woodward
-* @author: https://github.com/assemble
+* @author: https://github.com/doowb
 *
 * @param {[type]} params [description]
 * @param {Function} callback [description]
@@ -26,22 +26,12 @@ module.exports = function(params, callback) {
 
   'use strict';
 
-  var assemble       = params.assemble;
-  var grunt          = params.grunt;
-  var page           = params.page;
-  var content        = params.content;
-
-  var _str           = grunt.util._.str;
-  var _              = grunt.util._;
-
-
   // load current page content
-  var $ = cheerio.load(content);
+  var $ = cheerio.load(params.content);
   var toc = cheerio.load('<ul></ul>');
 
   // get all the anchor tags from inside the headers
   var anchors = $('h1 a[name],h2 a[name],h3 a[name],h4 a[name]');
-
   anchors.map(function(i, e) {
     var item = [
       '<li>',
@@ -49,14 +39,11 @@ module.exports = function(params, callback) {
       '</li>',
       ''
     ].join('\n');
-
     toc('ul').append(item);
-
   });
 
   $('body').prepend(toc.html());
   params.content = $.html();
-
   callback();
 };
 
